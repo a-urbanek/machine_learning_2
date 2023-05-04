40b193b9c6b984937743ea15ddc56082b78e66b7
Adrian Urbanek - Laptop <mail@adrian-urbanek.de>
2023-05-04 15:25:01 +0200

    Anmerkungen zu den einzelnen Codefragmenten

--- klassifikator_2.py
+++ klassifikator_2.py
@@ -4,6 +4,9 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 
+# Epoche: Alle Trainingsdaten 1 x sehen
+# Batch: x Daten aus dem Trainingsdatensatz sehen
+
 def train_mnist():
     # Lade den MNIST Datensatz                  // Datensatz wird zu Tensor umgewandelt
     train_set = torchvision.datasets.MNIST(
@@ -19,6 +22,7 @@ def train_mnist():
     # Erstelle Dataloader-Objekte            // Wird in "berechenbare" Objekte umgewandelt, Also 64 Bilder pro Datensatz
     train_loader = torch.utils.data.DataLoader(
         train_set, batch_size=64, shuffle=True
+
     )
 
     test_loader = torch.utils.data.DataLoader(
@@ -30,25 +34,33 @@ def train_mnist():
         def __init__(self):
             super(Net, self).__init__()
             self.fc1 = nn.Linear(784, 10)           # // Es wird nur eine "lineare" Schicht für das NN verwendet:
-                                                    # // 784 Eingänge, 10 Ausgänge
+                                                    # // 784 Eingänge, 10 Ausgänge -> repräsentieren also 10 Klassen
 
-        def forward(self, x):
-            x = x.view(-1, 784)
+        def forward(self, x):        # x = tensor
+            x = x.view(-1, 784)      # view(): macht aus x-dimensionalen Tensor (Matrix) einen eindimensionalen Vektor
+                                     # flatten(): wie view(), macht aus Tensor einen Vektor
             x = self.fc1(x)
             return x
 
     net = Net()
 
-    # Definiere Verlustfunktion und Optimierer                  # // Verlustfunktion: Differenz zwischen Vorhersage und
-                                                                # // tatsächlichem label --> Ziel: Differenz minimieren
+    # Definiere Loss-Funktion und Optimierer
+                                                                # Loss: Wie gut ist meine Hypothese (Modell)?
+                                                                # --> Ziel: Differenz minimieren
+
+                                                                # Gradient: partielle Ableitun des Loss-Wertes. Gilt
+                                                                # als Input für Optimizer.
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(net.parameters(), lr=0.01)      # // Optimizer: (???) Neues Test-Objekt für nächste
-                                                                # // Epoche?
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.01)      # // Optimizer: Wie ändere ich meine Parameter,
+                                                                # sodass ich besser werde (baasierend auf Loss-Wert
+                                                                # bzw. Gradient)? --> Strategie zum Parameter-Einstellen
+
+                                                                # Label: Ground-Truth (eigentliches Ergebnis des Test-Datensatzes)
 
     # Trainiere das Modell
-    for epoch in range(10):
+    for epoch in range(10):     # --> Epoch-Iteration
         running_loss = 0.0
-        for i, data in enumerate(train_loader, 0):
+        for i, data in enumerate(train_loader, 0):      # --> Batch-Iteration
             inputs, labels = data
 
             # Setze die Gradienten auf Null
@@ -86,3 +98,10 @@ def train_mnist():
         100 * correct / total))
 
 train_mnist()
+
+
+# Pipeline des ML:
+# 1. Datenvorbereitung (Daten Initialisieren, Daten-Loader erstellen)
+# 2. Modell erstellen (fully-connected-network)
+# 3. Training (Modell, Optimizer, Loss/Error/Fehler/Kosten-Funktion
+# 4. Evaluation (Testdaten, Modell)
